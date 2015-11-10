20ee8386daa504442ae2ce11ad5a5038573804f9
潘前进 <panqianjin@panqianjindeMacBook-Pro.local>
2015-11-10 16:11:38 +0800

    a

--- example/dialog.js
+++ example/dialog.js
@@ -4,12 +4,14 @@
 import React from 'react';
 import {Dialog} from '../index.js';
 React.render(
-    <Dialog type='dialog' title='重要提示' successCallback={function(){alert('a');}}>
-        <div style={{color:'#333'}}>
-            确认删除小肥羊华东自账户吗？
-            删除后将无法恢复
-            确认删除小肥羊华东自账户吗？
-            删除后将无法恢复
-        </div>
-    </Dialog>
+    <div>
+        <Dialog type='dialog' title='重要提示' show successCallback={function(){alert('a');}}>
+            <div style={{color:'#333'}}>
+                确认删除小肥羊华东自账户吗？
+                删除后将无法恢复
+                确认删除小肥羊华东自账户吗？
+                删除后将无法恢复
+            </div>
+        </Dialog>
+    </div>
     ,document.getElementById('root'));
