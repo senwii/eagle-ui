f7985842b78903b1975e711385d2b1a74f560b98
min.xiao@dianping.com <min.xiao@dianping.com>
2015-11-18 20:41:27 +0800

    update button and delete example dir

--- example/toast.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by panqianjin on 15/11/4.
- */
-import React from 'react';
-import {Toast} from '../index.js';
-React.render(
-    <Toast type='success' msg='保存成功' seconds='3' show  >
-    </Toast>
-    ,document.getElementById('root'));
--- examples/src/calendar.js
+++ examples/src/calendar.js
@@ -7,6 +7,7 @@ import Calendar from '../../lib/Calendar.js';
 import Input from '../../lib/Input.js';
 import Row from '../../lib/Row.js';
 import Col from '../../lib/Col.js';
+import Grid from '../../lib/Grid.js';
 import CalendarPanel from '../../lib/CalendarPanel.js';
 import querystring from 'querystring';
 
@@ -34,6 +35,7 @@ let Demo= class Demo extends Component{
     render(){
 
         return (
+            <Grid>
             <Row>
                 <Col sm={12}>
                     <CalendarPanel startDate="2015-11-11">
@@ -41,7 +43,18 @@ let Demo= class Demo extends Component{
                     </CalendarPanel>
 
                 </Col>
+                <Col sm={1} end>
+                    每日邮报报道,英国华威大学划艇队推出了2016年新版裸体日历,小伙子们在镜头前“坦诚相见”,大秀肌肉。这是该校划艇队第六次拍摄这种日历。 长期锻炼的队员们骄傲的
+                </Col>
             </Row>
+            <Row>
+                <Col sm={3}>
+                    <CalendarPanel startDate="2015-11-11" endDate="2015-12-1">
+                        <Input placeholder="请选择日期" style={{width:'150px'}} />
+                    </CalendarPanel>
+                </Col>
+            </Row>
+            </Grid>
         );
     }
 }
