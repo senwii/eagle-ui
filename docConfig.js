2612fc2b07a743a5a9da25146bbec25ea76bf683
ainiderenxm <277407493@qq.com>
2017-01-07 14:48:43 +0800

    doc

--- docConfig.js
+++ docConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     //扫描的文件路径
     paths: ['lib/'],
-    demoDir:"examples/src/modules/",
+    demoDir: "examples/src/modules/",
     //文档页面输出路径
     outdir: 'doc/',
     //内置主题
@@ -15,25 +15,24 @@ module.exports = {
         name: 'eagle-ui',
 
         //项目描述，可以配置html，会生成到document主页
-        description:
-        '<h3>欢迎使用eagle-ui，左侧的导航栏为相关react组件的名称和链接</h3>' +
-            '<p>&nbsp;</p>'+
+        description: '<h3>欢迎使用eagle-ui，左侧的导航栏为相关react组件的名称和链接</h3>' +
+            '<p>&nbsp;</p>' +
             '<h4>您可以点击顶部导航栏的<strong><a href="/documents/eagle-ui/examples/">示例</a></strong>，' +
-        '查看eagle-ui组件的UI效果及功能演示</h4>'+
-        '<p>&nbsp;</p>'+
-        '<h4>您可以通过如下方式查看组件的API</h4>'+
+            '查看eagle-ui组件的UI效果及功能演示</h4>' +
+            '<p>&nbsp;</p>' +
+            '<h4>您可以通过如下方式查看组件的API</h4>' +
 
-        '<ol>' +
-        '<li><h4>在输入框中输入组件名字进行查询</h4></li>' +
-        '<li><h4>点击左侧API导航栏</h4></li>' +
-        '</ol>'+
-            '<strong>说明:</strong>左侧导航栏中对应的链接名字即为组件名字'+
-        '<p>&nbsp;</p>'+
-            '<h4>您可以采用如下形式在项目中应用eagle-ui</h4>'+
-            '<pre>import {CheckboxGroup}(根据您的需要引入组件) from \'eagle-ui\'使用相关组件</pre>'+
-        '或者' +
-           '<pre>'+
-        '<div>&#60;script src="path to eagle-ui.js" &#62;&#60;/script&#62;的形式使用eagle-ui'+'</div></pre>'
+            '<ol>' +
+            '<li><h4>在输入框中输入组件名字进行查询</h4></li>' +
+            '<li><h4>点击左侧API导航栏</h4></li>' +
+            '</ol>' +
+            '<strong>说明:</strong>左侧导航栏中对应的链接名字即为组件名字' +
+            '<p>&nbsp;</p>' +
+            '<h4>您可以采用如下形式在项目中应用eagle-ui</h4>' +
+            '<pre>import {CheckboxGroup}(根据您的需要引入组件) from \'eagle-ui\'使用相关组件</pre>' +
+            '或者' +
+            '<pre>' +
+            '<div>&#60;script src="path to eagle-ui.js" &#62;&#60;/script&#62;的形式使用eagle-ui' + '</div></pre>'
 
         ,
 
@@ -45,14 +44,13 @@ module.exports = {
         //logo:'dp-logo.png',
 
         //导航信息
-        navs: [
-            {
+        navs: [{
                 name: "主页",
                 url: ""
             },
             {
                 name: "示例",
-                url: "/documents/eagle-ui/examples/"
+                url: "http://future-team.github.io/eagle-ui/examples/index.html"
             }
         ]
     },
@@ -60,17 +58,16 @@ module.exports = {
     demo: {
 
         ////外部资源链接
-        link : ['http://uedfamily.com/documents/eagle-ui/examples/js/eagle-ui.css'],
+        link: ['http://future-team.github.io/eagle-ui/examples/js/eagle-ui.css'],
 
         //文件复制路径; 将目下的资源复制到doc生成目录中，并在demo页面引用
         //paths : ['doc/modules/'],
 
         //是否开启在code编辑器中的自动完成功能(会将link和paths的引入加入)；默认开启；
-        autoComplete : true
+        autoComplete: true
     },
     ////自定义主题路径
     //themedir: 'docTheme/',
     //////自定义helpers
     //helpers: ["docTheme/dianping-theme.css","docTheme/dianping-theme.js"]
 };
-
