0c6afd0d3e7c7295b8853ae7b449840866f99325
genffy <lee.genffy@gmail.com>
2017-02-16 16:20:43 +0800

    update button

--- examples/src/modules/button.js
+++ examples/src/modules/button.js
@@ -3,6 +3,9 @@
  */
 import React, { Component ,PropTypes} from 'react';
 import {ButtonGroup,Button,Row,Col,Grid} from 'eagle-ui';
+import {DemoLayout, DemoItem, DemoShow, CodeShow} from '../libs/Layout';
+import Code, {getFile} from '../libs/Code';
+
 export default class ButtonCls extends Component{
     onActive(target,html){
         console.dir(html);
@@ -10,79 +13,75 @@ export default class ButtonCls extends Component{
     }
     render(){
         return (
-            <Grid>
-                <Row>
-                    <Col>
+            <DemoLayout title="按钮组件">
+                <DemoItem title="按钮样式">
+                    <CodeShow>
+                        <Code code={getFile('button-demo1')}/>
+                    </CodeShow>
+                    <DemoShow>
+                        <Button egStyle="warning">警告</Button>&nbsp;
+                        <Button egStyle="success">成功</Button>&nbsp;
+                        <Button egStyle="danger">危险</Button>&nbsp;
+                        <Button egStyle="error">错误</Button>&nbsp;
+                        <Button egStyle="info">info</Button>&nbsp;
+                        <Button >默认</Button>&nbsp;
+                        <Button egStyle="white">白色</Button>&nbsp;
+                        <Button egStyle="link" >链接</Button>
+                    </DemoShow>
+                </DemoItem>
+                <DemoItem title="按钮大小">
+                    <CodeShow>
+                        <Code code={getFile('button-demo2')}/>
+                    </CodeShow>
+                    <DemoShow>
+                        <Button egSize="xs" >小号</Button>&nbsp;
+                        <Button>默认</Button>&nbsp;
+                        <Button egSize="lg" >大号</Button>
+                    </DemoShow>
+                </DemoItem>
+                <DemoItem title="按钮组排列">
+                    <CodeShow>
+                        <Code code={getFile('button-demo3')}/>
+                    </CodeShow>
+                    <DemoShow>
                         <ButtonGroup egType="justify" activeCallback={this.onActive}>
-                            <Button radius egSize="sm" egStyle="warning">水平按钮组1</Button>
-                            <Button radius egSize="sm" egStyle="warning">水平按钮组2</Button>
-                            <Button radius egSize="sm" egStyle="warning">水平按钮组3</Button>
+                            <Button egStyle="warning">水平按钮组1</Button><Button egStyle="warning">水平按钮组2</Button><Button egStyle="warning">水平按钮组3</Button>
                         </ButtonGroup>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col sm={12}>
+                        <br/>
                         <ButtonGroup egType="tacked">
-                            <Button radius egSize="sm" egStyle="">垂直按钮组1</Button>
-                            <Button radius egSize="sm" egStyle="">垂直按钮组2</Button>
-                            <Button radius egSize="sm" egStyle="">垂直按钮组3</Button>
+                            <Button>垂直按钮组1</Button><Button>垂直按钮组2</Button><Button>垂直按钮组3</Button>
                         </ButtonGroup>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col sm={3}><Button radius egSize="sm" egStyle="warning">警告</Button></Col>
-                    <Col sm={3}><Button radius egSize="sm" egStyle="success">成功</Button></Col>
-                    <Col sm={3}><Button radius egSize="sm" egStyle="danger">危险</Button></Col>
-                    <Col sm={3}><Button radius egSize="sm" egStyle="error">错误</Button></Col>
-                </Row>
-                <Row>
-                    <Col sm={3}><Button radius egSize="sm" egStyle="info">info</Button></Col>
-                    <Col sm={3}><Button radius egSize="sm" >默认</Button></Col>
-                        <Col sm={3}><Button radius egSize="sm" egStyle="white">白色</Button></Col>
-                    <Col sm={3}><Button radius egSize="sm" egStyle="white" disabled>白色</Button></Col>
-                </Row>
-                <Row>
-                    <Col sm={3}><Button block radius egSize="sm" egStyle="gray">灰色</Button></Col>
-                    <Col sm={3}><Button block radius egSize="sm" >默认</Button></Col>
-                    <Col sm={3}><Button block radius egSize="sm" disabled>默认不可点</Button></Col>
-                    <Col sm={3}><Button block radius egSize="sm" egStyle="white" disabled>白色不可点</Button></Col>
-                </Row>
-                <Row>
-                    <Col sm={3}><Button block radius egSize="sm" egStyle="success" disabled>成功不可点</Button></Col>
-                    <Col sm={3}><Button block radius egSize="sm" >默认</Button></Col>
-                    <Col sm={3}><Button block radius egSize="sm" disabled>默认不可点</Button></Col>
-                    <Col sm={3}><Button block radius egSize="sm" egStyle="link" >链接</Button></Col>
-                </Row>
-                <Row>
-                    <Col sm={3}><Button block radius egSize="xs" egStyle="success" disabled>大号</Button></Col>
-                    <Col sm={3}><Button block radius egSize="xs" >大号</Button></Col>
-                    <Col sm={3}><Button block radius egSize="xs" disabled>大号</Button></Col>
-                    <Col sm={3}><Button block radius egSize="xs" egStyle="link" >大号</Button></Col>
-                </Row>
-                <Row>
-                    <Col sm={3}><Button block radius egSize="lg" egStyle="success" disabled>小号</Button></Col>
-                    <Col sm={3}><Button block radius egSize="lg" >小号</Button></Col>
-                    <Col sm={3}><Button block radius egSize="lg" disabled>小号</Button></Col>
-                    <Col sm={3}><Button block radius egSize="lg" egStyle="link" >小号</Button></Col>
-                </Row>
-                <Row>
-                    <Col sm={3}><Button block egSize="sm" egStyle="warning" hollow>警告</Button></Col>
-                    <Col sm={3}><Button block egSize="sm" egStyle="success" hollow>成功</Button></Col>
-                    <Col sm={3}><Button block egSize="sm" egStyle="danger" hollow>危险</Button></Col>
-                    <Col sm={3}><Button block egSize="sm" egStyle="error" hollow>错误</Button></Col>
-                </Row>
-                <Row>
-                    <Col sm={3}><Button block egSize="sm" egStyle="primary" hollow>primary</Button></Col>
-                    <Col sm={3}><Button block egSize="sm" egStyle="info" hollow>infp</Button></Col>
-                    <Col sm={3}><Button block egSize="sm" egStyle="default" hollow>default</Button></Col>
-                </Row>
-                <Row>
-                    <Col sm={3}><Button block egSize="sm" egStyle="warning" disabled hollow>警告</Button></Col>
-                    <Col sm={3}><Button block egSize="sm" egStyle="success" disabled hollow>成功</Button></Col>
-                    <Col sm={3}><Button block egSize="sm" egStyle="danger" disabled hollow>危险</Button></Col>
-                    <Col sm={3}><Button block egSize="sm" egStyle="error" disabled hollow>错误</Button></Col>
-                </Row>
-            </Grid>
+                    </DemoShow>
+                </DemoItem>
+                <DemoItem title="空心按钮">
+                    <CodeShow>
+                        <Code code={getFile('button-demo4')}/>
+                    </CodeShow>
+                    <DemoShow>
+                        <Button egStyle="warning" hollow>警告</Button>&nbsp;
+                        <Button egStyle="success" hollow>成功</Button>&nbsp;
+                        <Button egStyle="danger" hollow>危险</Button>&nbsp;
+                        <Button egStyle="error" hollow>错误</Button>&nbsp;
+                        <Button egStyle="white" hollow>white</Button>&nbsp;
+                        <Button egStyle="link" hollow>link</Button>
+                    </DemoShow>
+                </DemoItem>
+                <DemoItem title="默认／禁用">
+                    <CodeShow>
+                        <Code code={getFile('button-demo5')}/>
+                    </CodeShow>
+                    <DemoShow>
+                        <Button radius egSize="sm">默认</Button>&nbsp;
+                        <Button radius egSize="sm" disabled>默认不可点</Button>
+                        <br/><br/>
+                        <Button egStyle="white">白色</Button>&nbsp;
+                        <Button egStyle="white" disabled>白色不可点</Button>
+                        <br/><br/>
+                        <Button egStyle="success">成功</Button>&nbsp;
+                        <Button egStyle="success" disabled>成功不可点</Button>
+                    </DemoShow>
+                </DemoItem>
+            </DemoLayout>
         );
     }
 }
